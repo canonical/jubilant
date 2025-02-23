a3af8bb2ad90859eb06ec2e153c29645e15bc040
Ben Hoyt <ben.hoyt@canonical.com>
2025-02-24 12:33:19 +1300

    test: add status test with real JSON from Juju

--- test/unit/test_status.py
+++ test/unit/test_status.py
@@ -47,6 +47,74 @@ def test_minimal_with_model(run: mocks.Run):
     assert status == MINIMAL_STATUS
 
 
-def test_realistic():
-    # TODO: get some JSON from a real Juju
-    pass
+def test_real_status(run: mocks.Run):
+    status_json = """
+{
+    "model": {
+        "name": "tt",
+        "type": "caas",
+        "controller": "microk8s-localhost",
+        "cloud": "microk8s",
+        "region": "localhost",
+        "version": "3.6.1",
+        "model-status": {
+            "current": "available",
+            "since": "24 Feb 2025 12:02:57+13:00"
+        },
+        "sla": "unsupported"
+    },
+    "machines": {},
+    "applications": {
+        "snappass-test": {
+            "charm": "snappass-test",
+            "base": {
+                "name": "ubuntu",
+                "channel": "20.04"
+            },
+            "charm-origin": "charmhub",
+            "charm-name": "snappass-test",
+            "charm-rev": 9,
+            "charm-channel": "latest/stable",
+            "scale": 1,
+            "provider-id": "276bec9f-6a0c-46ea-8094-aca6337d46e5",
+            "address": "10.152.183.248",
+            "exposed": false,
+            "application-status": {
+                "current": "active",
+                "message": "snappass started",
+                "since": "24 Feb 2025 12:03:17+13:00"
+            },
+            "units": {
+                "snappass-test/0": {
+                    "workload-status": {
+                        "current": "active",
+                        "message": "snappass started",
+                        "since": "24 Feb 2025 12:03:17+13:00"
+                    },
+                    "juju-status": {
+                        "current": "idle",
+                        "since": "24 Feb 2025 12:03:18+13:00",
+                        "version": "3.6.1"
+                    },
+                    "leader": true,
+                    "address": "10.1.164.138",
+                    "provider-id": "snappass-test-0"
+                }
+            }
+        }
+    },
+    "storage": {},
+    "controller": {
+        "timestamp": "12:04:55+13:00"
+    }
+}
+"""
+    run.handle(['juju', 'status', '--format', 'json'], stdout=status_json)
+    juju = jubilant.Juju()
+
+    status = juju.status()
+
+    assert status.model.type == 'caas'
+    assert status.apps['snappass-test'].is_active
+    assert status.apps['snappass-test'].units['snappass-test/0'].is_active
+    assert status.apps['snappass-test'].units['snappass-test/0'].leader
