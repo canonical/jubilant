89a62c655faf48b1d2166f4a721b1c89cc469f07
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-01 21:38:10 +1300

    Revert "feat: add run command"
    This reverts commit 99296c3e324180fec975d88fec662454a12cdf5c.

--- jubilant/__init__.py
+++ jubilant/__init__.py
@@ -1,7 +1,6 @@
 """Jubilant is a Pythonic wrapper around the Juju CLI for integration testing."""
 
 from . import statustypes
-from ._actions import ActionError, ActionResult
 from ._all_any import (
     all_active,
     all_blocked,
@@ -18,8 +17,6 @@ from ._juju import CLIError, ConfigValue, Juju, SecretURI, WaitError
 from .statustypes import Status
 
 __all__ = [
-    'ActionError',
-    'ActionResult',
     'CLIError',
     'ConfigValue',
     'Juju',
