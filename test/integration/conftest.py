ed6aa761fb2fa4415cdb9a4b2b35a5f742618992
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-12 15:58:45 +1300

    simplify juju fixture (no --model); print debug-logs on failure

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -8,10 +8,6 @@ import jubilant
 
 def pytest_addoption(parser: pytest.OptionGroup):
     parser.addoption(
-        '--model',
-        help='existing model to use for tests (default is to create a randomly-named one per test)',
-    )
-    parser.addoption(
         '--keep-models',
         action='store_true',
         default=False,
@@ -23,12 +19,11 @@ def pytest_addoption(parser: pytest.OptionGroup):
 def juju(request: pytest.FixtureRequest) -> Generator[jubilant.Juju, None, None]:
     """Pytest fixture that wraps :meth:`jubilant.with_model`.
 
-    This adds command line parameters ``--model`` and ``--keep-models`` (see help for details).
+    This adds command line parameter ``--keep-models`` (see help for details).
     """
-    model = cast(str | None, request.config.getoption('--model'))
     keep_models = cast(bool, request.config.getoption('--keep-models'))
-    if model:
-        yield jubilant.Juju(model=model)
-    else:
-        with jubilant.temp_model(keep=keep_models) as juju:
-            yield juju
+    with jubilant.temp_model(keep=keep_models) as juju:
+        yield juju
+        if request.session.testsfailed:
+            log = juju.debug_log(limit=1000)
+            print(log, end='')
