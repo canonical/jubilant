1997303bf299e315f2f22e3a2f407ddbc2f95526
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-12 16:08:36 +1300

    revert test failure

--- test/integration/test_basic.py
+++ test/integration/test_basic.py
@@ -4,4 +4,4 @@ import jubilant
 def test_deploy(juju: jubilant.Juju):
     juju.deploy('snappass-test')
 
-    juju.wait(jubilant.all_error, timeout=60) # TODO: revert
+    juju.wait(jubilant.all_active)
