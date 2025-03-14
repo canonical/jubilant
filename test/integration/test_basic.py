c472f294a87c9732c349c5b498d0203a909a8e66
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-14 17:32:35 +1300

    use test charms for integration tests

--- test/integration/test_basic.py
+++ test/integration/test_basic.py
@@ -1,6 +1,10 @@
+import pathlib
+
 import requests
 
 import jubilant
+
+CHARMS_PATH = pathlib.Path(__file__).parent / 'charms'
 
 
 def test_deploy(juju: jubilant.Juju):
@@ -29,34 +33,43 @@ def test_add_and_remove_unit(juju: jubilant.Juju):
     )
 
 
-def test_config(juju: jubilant.Juju):
-    juju.deploy('wordpress-k8s')
-    juju.wait(jubilant.all_blocked)  # Waiting for db relation/config
+def test_config_and_run(juju: jubilant.Juju):
+    charms = [p.absolute() for p in (CHARMS_PATH / 'testdb').glob('*.charm')]
+    assert charms, 'testdb .charm file not found'
+    juju.deploy(charms[0])
+    juju.wait(jubilant.all_blocked)  # waiting for relation
 
-    config = juju.config('wordpress-k8s')
-    assert config['blog_hostname'] == ''
+    config = juju.config('testdb')
+    assert config['testoption'] == ''
 
-    app_config = juju.config('wordpress-k8s', app_config=True)
+    app_config = juju.config('testdb', app_config=True)
     assert app_config['trust'] is False
 
-    juju.config('wordpress-k8s', {'blog_hostname': 'example.com'})
-    config = juju.config('wordpress-k8s')
-    assert config['blog_hostname'] == 'example.com'
+    juju.config('testdb', {'testoption': 'foobar'})
+    config = juju.config('testdb')
+    assert config['testoption'] == 'foobar'
+
+    # TODO: debug this. The --params /tmp/file doesn't seem to work. Maybe /tmp not available?
+    # result = juju.run('testdb/0', 'do-thing', {'param1': 'value1'})
+    # assert result.success
+    # assert result.return_code == 0
+    # assert result.results == {
+    #     'config': {'testoption': 'foobar'},
+    #     'params': {'param1': 'value1'},
+    #     'thingy': 'foo',
+    # }
 
 
-def test_integrate_and_run(juju: jubilant.Juju):
-    juju.deploy('wordpress-k8s')
-    juju.deploy('mysql-k8s')
-    juju.integrate('wordpress-k8s', 'mysql-k8s')
-    status = juju.wait(jubilant.all_active, timeout=20 * 60)
+def test_integrate(juju: jubilant.Juju):
+    charms = [p.absolute() for p in (CHARMS_PATH / 'testdb').glob('*.charm')]
+    assert charms, 'testdb .charm file not found'
+    juju.deploy(charms[0])
 
-    address = status.apps['wordpress-k8s'].units['wordpress-k8s/0'].address
-    response = requests.get(f'http://{address}/', timeout=10)
-    response.raise_for_status()
-    assert '<title>' in response.text
-    assert 'wordpress' in response.text.lower()
+    charms = [p.absolute() for p in (CHARMS_PATH / 'testdb').glob('*.charm')]
+    assert charms, 'testdb .charm file not found'
+    juju.deploy(charms[0])
 
-    result = juju.run('wordpress-k8s/0', 'get-initial-password')
-    assert result.success
-    assert result.return_code == 0
-    assert len(result.results['password']) >= 8
+    juju.integrate('testdb', 'testapp')
+    status = juju.wait(jubilant.all_active)
+    assert status.apps['testdb'].app_status.current == 'relation created'
+    assert status.apps['testapp'].app_status.current == 'relation changed: dbkey=dbvalue'
