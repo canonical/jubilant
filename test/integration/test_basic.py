a1c4581b633d19b3d65afb4e66c5df69c36ee8ec
Ben Hoyt <ben.hoyt@canonical.com>
2025-03-19 17:40:19 +1300

    factor out charm name in integration tests

--- test/integration/test_basic.py
+++ test/integration/test_basic.py
@@ -8,10 +8,11 @@ CHARMS_PATH = pathlib.Path(__file__).parent / 'charms'
 
 
 def test_deploy(juju: jubilant.Juju):
-    juju.deploy('snappass-test')
+    charm = 'snappass-test'
+    juju.deploy(charm)
     status = juju.wait(jubilant.all_active)
 
-    address = status.apps['snappass-test'].units['snappass-test/0'].address
+    address = status.apps[charm].units[charm + '/0'].address
     response = requests.get(f'http://{address}:5000/', timeout=10)
     response.raise_for_status()
     assert '<title>' in response.text
@@ -19,39 +20,37 @@ def test_deploy(juju: jubilant.Juju):
 
 
 def test_add_and_remove_unit(juju: jubilant.Juju):
-    juju.deploy('snappass-test')
+    charm = 'snappass-test'
+    juju.deploy(charm)
     juju.wait(jubilant.all_active)
 
-    juju.add_unit('snappass-test')
-    juju.wait(
-        lambda status: jubilant.all_active(status) and len(status.apps['snappass-test'].units) == 2
-    )
+    juju.add_unit(charm)
+    juju.wait(lambda status: jubilant.all_active(status) and len(status.apps[charm].units) == 2)
 
-    juju.remove_unit('snappass-test', num_units=1)
-    juju.wait(
-        lambda status: jubilant.all_active(status) and len(status.apps['snappass-test'].units) == 1
-    )
+    juju.remove_unit(charm, num_units=1)
+    juju.wait(lambda status: jubilant.all_active(status) and len(status.apps[charm].units) == 1)
 
 
 def test_config_and_run(juju: jubilant.Juju):
-    juju.deploy(charm_path('testdb'))
+    charm = 'testdb'
+    juju.deploy(charm_path(charm))
 
     # unit should come up as "unknown"
     juju.wait(
-        lambda status: status.apps['testdb'].units['testdb/0'].workload_status.current == 'unknown'
+        lambda status: status.apps[charm].units[charm + '/0'].workload_status.current == 'unknown'
     )
 
-    config = juju.config('testdb')
+    config = juju.config(charm)
     assert config['testoption'] == ''
 
-    app_config = juju.config('testdb', app_config=True)
+    app_config = juju.config(charm, app_config=True)
     assert app_config['trust'] is False
 
-    juju.config('testdb', {'testoption': 'foobar'})
-    config = juju.config('testdb')
+    juju.config(charm, {'testoption': 'foobar'})
+    config = juju.config(charm)
     assert config['testoption'] == 'foobar'
 
-    result = juju.run('testdb/0', 'do-thing', {'param1': 'value1'})
+    result = juju.run(charm + '/0', 'do-thing', {'param1': 'value1'})
     assert result.success
     assert result.return_code == 0
     assert result.results == {
