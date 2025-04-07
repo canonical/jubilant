8de10cc9611b16587a10db8c131dffa89aee715b
David Wilding <david.wilding@canonical.com>
2025-04-07 13:32:22 +0800

    Added implementation and single unit test

---
+++ tests/unit/test_remove_application.py
@@ -0,0 +1,12 @@
+import pytest
+
+import jubilant
+
+from . import mocks
+
+
+def test_single(run: mocks.Run):
+    run.handle(['juju', 'remove-application', '--no-prompt', 'app1'])
+    juju = jubilant.Juju()
+
+    juju.remove_application('app1')
