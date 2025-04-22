dd1f34cd5a30430aae31c1114a44b06e2c2e5935
Ben Hoyt <ben.hoyt@canonical.com>
2025-04-22 14:38:34 +1200

    fix formatting

--- tests/unit/test_scp.py
+++ tests/unit/test_scp.py
@@ -11,7 +11,20 @@ def test_minimal(run: mocks.Run):
 
 
 def test_all_args(run: mocks.Run):
-    run.handle(['juju', 'scp', '--container', 'redis', '--no-host-key-checks', '--', '-r', '-C', 'SRC', 'DST'])
+    run.handle(
+        [
+            'juju',
+            'scp',
+            '--container',
+            'redis',
+            '--no-host-key-checks',
+            '--',
+            '-r',
+            '-C',
+            'SRC',
+            'DST',
+        ]
+    )
     juju = jubilant.Juju()
 
     juju.scp('SRC', 'DST', container='redis', host_key_checks=False, scp_options=['-r', '-C'])
