bc0112518d57023aa14fe103c24edaf2c736ecb5
Paulo Machado <paulo.machado@canonical.com>
2025-04-30 00:30:48 -0300

    fix bind mapping command format

--- jubilant/_juju.py
+++ jubilant/_juju.py
@@ -364,8 +364,9 @@ class Juju:
             args.extend(['--base', base])
         if bind is not None:
             if not isinstance(bind, str):
-                bind = ','.join(f'{k}={v}' for k, v in bind.items())
-            args.extend(['--bind', bind])
+                args.extend([f'--bind={k}={v}' for k, v in bind.items()])
+            else:
+                args.extend(['--bind', bind])
         if channel is not None:
             args.extend(['--channel', channel])
         if config is not None:
