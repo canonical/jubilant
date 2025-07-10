a8ff969d601a8a3451d9e641c8579a682a1b8c01
Ben Hoyt <ben.hoyt@canonical.com>
2025-07-10 16:41:34 -0400

    update test

--- tests/integration/test_cross_model.py
+++ tests/integration/test_cross_model.py
@@ -9,29 +9,14 @@ def test_offer_and_consume(juju: jubilant.Juju, model2: jubilant.Juju):
     juju.deploy(helpers.find_charm('testdb'))
     juju.offer(f'{juju.model}.testdb', endpoint='db', name='testdbx')
 
-    out = juju.cli('offers')
-    print('OFFERS1\n' + out)
-
     model2.deploy(helpers.find_charm('testapp'))
     model2.consume(f'{juju.model}.testdbx', 'dbalias')
-
-    out = model2.cli('offers')
-    print('OFFERS2\n' + out)
-
     model2.integrate('dbalias', 'testapp')
 
-    out = juju.cli('offers')
-    print('OFFERS1b\n' + out)
-
     status = juju.wait(jubilant.all_active)
-    #    assert status.apps['testdb'].relations['db'][0].related_app == 'testapp'
     assert status.apps['testdb'].app_status.message == 'relation created'
 
     status2 = model2.wait(jubilant.all_active)
-    #    assert status2.apps['testapp'].relations['db'][0].related_app == 'testdb'
+    assert status2.apps['testapp'].relations['db'][0].related_app == 'dbalias'
     assert status2.apps['testapp'].app_status.message == 'relation changed: dbkey=dbvalue'
-
-    out = juju.cli('offers')
-    print('OFFERS1c\n' + out)
-
-    assert 1 == 2
+    assert status2.app_endpoints['dbalias'].relations['db'][0] == 'testapp'
