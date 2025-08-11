ec0e84dd0993c758e272f8a58b95a084409ec2e2
David Wilding <david.wilding@canonical.com>
2025-08-11 08:02:36 +0800

    add CSS for cookie banner

--- docs/conf.py
+++ docs/conf.py
@@ -288,7 +288,9 @@ exclude_patterns = [
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
+html_css_files = [
+    "cookie-banner.css",
+]
 
 
 # Adds custom JavaScript files, located under 'html_static_path'
