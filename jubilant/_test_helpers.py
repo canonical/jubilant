bff29e5a65a31590a65b128f13ac8f48b5af59a7
Ben Hoyt <ben.hoyt@canonical.com>
2025-10-08 16:20:14 +1300

    add comments per review

--- jubilant/_test_helpers.py
+++ jubilant/_test_helpers.py
@@ -25,6 +25,9 @@ def temp_model(
     This creates a new model with a random name in the format ``jubilant-abcd1234``, and destroys
     it and its storage when the context manager exits.
 
+    If destroying the model takes longer than 10 minutes, log an error and return successfully.
+    This is mainly to work around issues with Microk8s destroy-model hanging indefinitely.
+
     Provides a :class:`Juju` instance to operate on.
 
     If you want to configure its parameters, such as ``wait_timeout``, set the appropriate
@@ -54,6 +57,8 @@ def temp_model(
         if not keep:
             assert juju.model is not None
             try:
+                # We're not using juju.destroy_model() here, as it doesn't have a timeout
+                # parameter. If we add such a parameter, we can update this.
                 args = ['destroy-model', juju.model, '--no-prompt', '--destroy-storage', '--force']
                 juju._cli(*args, include_model=False, timeout=10 * 60)
                 juju.model = None
