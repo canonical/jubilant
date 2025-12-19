2e3051d93d49246addbc35fe5eca592a32bfd138
Soundarya03 <soundarya1103@gmail.com>
2025-12-19 15:28:56 +0530

    feat: defines model_constraints method to set/get machine constraints

---
+++ tests/unit/test_model_constraints.py
@@ -0,0 +1,51 @@
+import jubilant
+from tests.unit import mocks
+
+CONSTRAINTS = '{"arch":"amd64","cores":8,"mem":16384}'
+
+
+def test_get(run: mocks.Run):
+    run.handle(['juju', 'model-constraints', '--format', 'json'], stdout=CONSTRAINTS)
+
+    juju = jubilant.Juju()
+    values = juju.model_constraints()
+    assert values == {'arch': 'amd64', 'cores': 8, 'mem': 16384}
+
+
+def test_get_with_model(run: mocks.Run):
+    run.handle(
+        ['juju', 'model-constraints', '--format', 'json', '--model', 'test-model'],
+        stdout=CONSTRAINTS,
+    )
+
+    juju = jubilant.Juju()
+    values = juju.model_constraints(model='test-model')
+    assert values == {'arch': 'amd64', 'cores': 8, 'mem': 16384}
+
+
+def test_set(run: mocks.Run):
+    run.handle(['juju', 'set-model-constraints', 'arch=amd64', 'cores=8', 'mem=16384'])
+
+    juju = jubilant.Juju()
+    values = juju.model_constraints(constraints={'arch': 'amd64', 'cores': '8', 'mem': '16384'})
+    assert values is None
+
+
+def test_set_with_model(run: mocks.Run):
+    run.handle(
+        [
+            'juju',
+            'set-model-constraints',
+            'arch=amd64',
+            'cores=8',
+            'mem=16384',
+            '--model',
+            'test-model',
+        ]
+    )
+
+    juju = jubilant.Juju()
+    values = juju.model_constraints(
+        model='test-model', constraints={'arch': 'amd64', 'cores': '8', 'mem': '16384'}
+    )
+    assert values is None
