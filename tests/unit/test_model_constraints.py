904f6ee9b4ac068d384ca810e7148fc536ebacdd
Soundarya03 <soundarya1103@gmail.com>
2026-01-05 15:52:09 +0530

    refactor: minor fixes

--- tests/unit/test_model_constraints.py
+++ tests/unit/test_model_constraints.py
@@ -1,11 +1,14 @@
+import pytest
+
 import jubilant
 from tests.unit import mocks
 
-CONSTRAINTS = '{"arch":"amd64","cores":8,"mem":16384}'
-
 
 def test_get(run: mocks.Run):
-    run.handle(['juju', 'model-constraints', '--format', 'json'], stdout=CONSTRAINTS)
+    run.handle(
+        ['juju', 'model-constraints', '--format', 'json'],
+        stdout='{"arch":"amd64","cores":8,"mem":16384}',
+    )
 
     juju = jubilant.Juju()
     values = juju.model_constraints()
@@ -13,8 +16,25 @@ def test_get(run: mocks.Run):
 
 
 def test_set(run: mocks.Run):
-    run.handle(['juju', 'set-model-constraints', 'arch=amd64', 'cores=8', 'mem=16384'])
+    run.handle(
+        [
+            'juju',
+            'set-model-constraints',
+            'arch=amd64',
+            'cores=8',
+            'mem=16384',
+            'allocate-public-ip=false',
+        ]
+    )
 
     juju = jubilant.Juju()
-    values = juju.model_constraints(constraints={'arch': 'amd64', 'cores': '8', 'mem': '16384'})
-    assert values is None
+    retval = juju.model_constraints(
+        {'arch': 'amd64', 'cores': 8, 'mem': '16384', 'allocate-public-ip': False}
+    )
+    assert retval is None
+
+
+def test_format_constraint_type_error():
+    juju = jubilant.Juju()
+    with pytest.raises(TypeError):
+        juju.model_constraints({'foo': None})  # type: ignore
