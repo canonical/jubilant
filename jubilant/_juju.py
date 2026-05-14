de01a1a749cf34af92ca4dba76ee74aa2e34e99d
Trong Nhan Mai <trongnhan.mai@canonical.com>
2026-05-14 10:53:13 +1000

    chore: add a small comment explaining the fix in _deploy_tempdir

--- jubilant/_juju.py
+++ jubilant/_juju.py
@@ -1690,6 +1690,9 @@ class Juju:
         resources: Mapping[str, str] | None,
     ) -> Generator[tuple[str | None, Mapping[str, str] | None]]:
         if isinstance(charm, pathlib.Path):
+            # We add "./" to any relative pathlib.Path objects in their string form.
+            # pathlib.Path removes the redundant "./", which we need to prevent Juju
+            # from saying the path is ambiguos.
             charm = str(charm) if charm.is_absolute() else f'./{charm}'
         charm_needs_temp = charm is not None and charm.startswith(('.', '/'))
         resources_needs_temp = resources is not None and any(
