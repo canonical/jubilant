e4431f8f81e75b25cb9d6db501a23e97af0d830c
Trong Nhan Mai <trongnhan.mai@canonical.com>
2026-05-20 09:54:43 +1000

    fix: add ./ for relative pathlib.Path objects in _deploy_tempdir (#317)
    Fixes #277 .
    
    This PR modifies how `_deploy_tempdir` handles the `charm` parameter
    when it is a `pathlib.Path` object.
    - If `charm` is an absolute path, we coerce it to a string, preserving
    its original path.
    - If `charm` is a relative path, we coerce it to a string, and append
    `./` to the beginning.
    
    I used `is_absolute()` instead of `root` to decide if a `pathlib.Path`
    object is absolute or not.
    
    I added unit tests for deploying and refreshing from relative
    `pathlib.Path` objects.

--- jubilant/_juju.py
+++ jubilant/_juju.py
@@ -1689,8 +1689,11 @@ class Juju:
         charm: str | pathlib.Path | None,
         resources: Mapping[str, str] | None,
     ) -> Generator[tuple[str | None, Mapping[str, str] | None]]:
-        if charm is not None:
-            charm = str(charm)
+        if isinstance(charm, pathlib.Path):
+            # We add "./" to any relative pathlib.Path objects in their string form.
+            # pathlib.Path removes the redundant "./", which we need to prevent Juju
+            # from saying the path is ambiguous.
+            charm = str(charm) if charm.is_absolute() else f'./{charm}'
         charm_needs_temp = charm is not None and charm.startswith(('.', '/'))
         resources_needs_temp = resources is not None and any(
             v.startswith(('.', '/')) for v in resources.values()
