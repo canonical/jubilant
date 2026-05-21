a3b8bfffac8de9957cee846892961ea051550771
David Wilding <david.wilding@canonical.com>
2026-05-21 10:43:42 +0800

    fix linkcheck regex

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ linkcheck_ignore = [
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
 linkcheck_anchors_ignore_for_url = [
     r"https://github\.com/.*",
-    r"https://matrix.to/.*",
+    r"https://matrix\.to/.*",
 ]
 
 # How long the link checker will wait for a response for each request
