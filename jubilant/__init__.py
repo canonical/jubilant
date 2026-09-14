2b949d2a864d9e3abd480596077a22dd0bf7d85f
Neha Oudin <neha@oudin.red>
2026-09-14 10:46:08 +0200

    feat: Export ConstraintValue in module
    Can be useful for methods wrapping deploys or set/update model constraints
    
    fixes: #412

--- jubilant/__init__.py
+++ jubilant/__init__.py
@@ -14,7 +14,7 @@ from ._all_any import (
     any_maintenance,
     any_waiting,
 )
-from ._juju import CLIError, ConfigValue, Juju, WaitError
+from ._juju import CLIError, ConfigValue, ConstraintValue, Juju, WaitError
 from ._task import Task, TaskError
 from ._test_helpers import temp_model
 from ._version import Version
@@ -26,6 +26,7 @@ from .unittypes import UnitInfo
 __all__ = [
     'CLIError',
     'ConfigValue',
+    'ConstraintValue',
     'Juju',
     'ModelInfo',
     'RevealedSecret',
@@ -55,4 +56,4 @@ __all__ = [
     'unittypes',
 ]
 
-__version__ = '1.13.0'
+__version__ = '1.14.0'
